5371bffd399e7d69077e2645efbeb782936e6a88
Tejas0801 <tejakandhikatla97@gmail.com>
2024-03-05 04:50:43 +0530

    update

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,56 +1,38 @@
 <?php
-mb_internal_encoding("UTF-8");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields and remove whitespace
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
 
-$to = 'snani.1997@gmail.com';
-$subject = 'Message from Fortune';
+    // Set your email address where you want to receive emails
+    $to = "snani.1997@gmail.com"; // <-- Replace "your_email@example.com" with your actual email address
 
-$name = "";
-$email = "";
-$phone = "";
-$message = "";
+    // Set the subject line for the email
+    $subject = "New Contact Form Submission from $name";
 
-if( isset($_POST['name']) ){
-    $name = $_POST['name'];
-    //echo $name;
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
 
-    $body .= "Name: ";
-    $body .= $name;
-    $body .= "\n\n";
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($to, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request, set a 403 (forbidden) response code
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
 }
-if( isset($_POST['email']) ){
-    $email = $_POST['email'];
-    //echo $email;
-
-    $body .= "";
-    $body .= "Email: ";
-    $body .= $email;
-    $body .= "\n\n";
-}
-if( isset($_POST['phone']) ){
-    $phone = $_POST['phone'];
-    //echo $phone;
-
-    $body .= "";
-    $body .= "Phone: ";
-    $body .= $phone;
-    $body .= "\n\n";
-}
-if( isset($_POST['message']) ){
-    $message = $_POST['message'];
-    //echo $message;
-
-    $body .= "";
-    $body .= "Message: ";
-    $body .= $message;
-    $body .= "\n\n";
-}
-
-$headers = 'From: ' .$email . "\r\n";
-
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-mb_send_mail($to, $subject, $body, $headers);
-    echo '<div class="status-icon valid"><i class="icon_check"></i></div>';
-}
-else{
-    echo '<div class="status-icon invalid"><i class="icon_close"></i></div>';
-}
+?>

45417549789b0b83d403c268561d2d79dcd17185
Tejas0801 <tejakandhikatla97@gmail.com>
2024-04-16 12:11:45 +0530

    update
    update

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,32 +1,21 @@
 <?php
-phpinfo();
-mb_internal_encoding("UTF-8");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-$to = ''; // <-- Replace 'snani.1997@gmail.com' with your actual email address
-$subject = 'Message from Fortune';
+    // Set up email
+    $to = "snani.1997@gmail.com"; // Your email address
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
 
-$name = isset($_POST['name']) ? $_POST['name'] : '';
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
-$message = isset($_POST['message']) ? $_POST['message'] : '';
-
-$body = "Name: $name\n";
-$body .= "Email: $email\n";
-$body .= "Phone: $phone\n";
-$body .= "Message: $message\n";
-
-$headers = "From: $email\r\n";
-
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    if (mb_send_mail($to, $subject, $body, $headers)) {
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
+    // Send email
+    if (mail($to, $subject, $body)) {
+        echo "success";
     } else {
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
+        echo "error";
     }
-} else {
-    http_response_code(400);
-    echo "Invalid email address.";
 }
 ?>
+
